62243a02e319cee4eb6634009c43cbba56b9d3ce
HPreast <preasths1@gmail.com>
2021-05-21 13:32:26 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.Write("First Name> ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+// string initials = firstName[0] + " " + lastName[0];
+// Console.WriteLine(initials);
+
+
+Console.WriteLine(firstName + " " + lastName);
+
+string reverse = string.Empty;
+string fullName = firstName + " " + lastName;
+
+for (int i = fullName.Length - 1; i >= 0; i--)
+{
+    reverse += fullName[i];
+}
+Console.WriteLine(reverse);
